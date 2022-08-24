e50ef7bb2272fad03a9d588dad50da9c6eecec17
Seiya-Tagami <bepozurapu274@gmail.com>
2022-08-24 16:22:48 +0900

    プルリク一部修正

--- Memory Game/main.js
+++ Memory Game/main.js
@@ -1,93 +1,91 @@
-'use strict'
+"use strict";
 {
   const cards = [];
-  const cardBox = document.getElementById('p-card-box');
+  const cardBox = document.getElementById("p-card-box");
   let flipCount = 0,
-  correctCount = 0,
-  countUp = 0,
-  firstCard = null,
-  secondCard = null,
-  countDown = 3;
-  
-  function init(){
+    correctCount = 0,
+    countUp = 0,
+    firstCard = null,
+    secondCard = null,
+    countDown = 3;
 
-    for(let i = 1; i <= 8; i++){
+  function init() {
+    for (let i = 1; i <= 8; i++) {
       cards[cards.length] = createCard(i);
       cards[cards.length] = createCard(i);
-    };
+    }
 
-    while(cards.length){
-      const pos = Math.floor(Math.random()*cards.length);
+    while (cards.length) {
+      const pos = Math.floor(Math.random() * cards.length);
       cardBox.appendChild(cards.splice(pos, 1)[0]); //memo:spliceの返値は配列
-    };
+    }
   }
-  
+
   function createCard(num) {
-    const inner ='<div class="card-back">？</div><div class="card-front">*</div>'; 
-    const card = document.createElement('div');
-    card.className = 'card';
-    card.innerHTML = inner.replace('*',num);
-    card.addEventListener('click', function(){ 
+    const inner =
+      '<div class="card-back">？</div><div class="card-front">*</div>';
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = inner.replace("*", num);
+    card.addEventListener("click", function () {
       flipCard(this);
     });
-    const container = document.createElement('div');
-    container.className ='card-container';
+    const container = document.createElement("div");
+    container.className = "card-container";
     container.appendChild(card);
-    return container; 
+    return container;
     //memo:もしreturnがないと関数内で処理を行うのみで終了になってしまう。戻り値があること処理結果を別の処理で使いまわすことができる。
   }
 
   function flipCard(card) {
-    if(firstCard != null && secondCard != null){
+    if (firstCard != null && secondCard != null) {
       return;
     }
 
-    if(card.className.indexOf("open") === -1){
+    if (card.className.indexOf("open") === -1) {
       card.className = "card open";
     } else {
       return;
     }
     flipCount++;
-    if(flipCount % 2 === 1 ) {
+    if (flipCount % 2 === 1) {
       firstCard = card;
     } else {
       secondCard = card;
-      setTimeout(function() {
+      setTimeout(function () {
         judge();
       }, 900); //memo:カードをめくり終わった時に実行
     }
   }
 
   function judge() {
-    if(firstCard.children[1].textContent === secondCard.children[1].textContent){
+    if (
+      firstCard.children[1].textContent === secondCard.children[1].textContent
+    ) {
       correctCount++;
     } else {
-      firstCard.className = 'card';
-      secondCard.className = 'card';
+      firstCard.className = "card";
+      secondCard.className = "card";
     }
     firstCard = null;
-    secondCard = null; 
+    secondCard = null;
   }
 
-
-
-  const start = document.querySelector('.p-start');
-  const replay = document.querySelector('.p-sub__item__replay');
-  const clickReplay =  document.getElementById('js-replay');
-  document.getElementById('soloPlay').addEventListener('click', function(){
-    start.style.display = 'none';
+  const start = document.querySelector(".p-start");
+  const replay = document.querySelector(".p-sub__item__replay");
+  const clickReplay = document.getElementById("js-replay");
+  document.getElementById("js-soloPlay").addEventListener("click", function () {
+    start.style.display = "none";
     gameStart();
-    clickReplay.addEventListener('click', function(){
+    clickReplay.addEventListener("click", function () {
       rePlay();
-    })
-  })
+    });
+  });
 
+  const timer1 = document.querySelector(".p-sub__item__count-down");
 
-
-  const timer1 = document.querySelector('.p-sub__item__count-down');
-
-  function gameStart(){
-    if(clickReplay.classList.contains('stop')){
+  function gameStart() {
+    if (clickReplay.classList.contains("stop")) {
       return;
     }
 
@@ -96,76 +94,73 @@
     countDown--;
     const cleartimeoutId = setTimeout(gameStart, 1000);
 
-    if(countDown === -1){
+    if (countDown === -1) {
       clearTimeout(cleartimeoutId);
-      timer1.innerHTML = '始め！'
-      replay.style.display ='flex';
+      timer1.innerHTML = "始め！";
+      replay.style.display = "flex";
       init();
-      clickReplay.classList.remove('stop2');
+      clickReplay.classList.remove("stop2");
       countTime();
     }
   }
 
-  function countTime(){
-    if(clickReplay.classList.contains('stop2')){
+  function countTime() {
+    if (clickReplay.classList.contains("stop2")) {
       return;
     }
-    const timer2 = document.querySelector('.p-sub__item__count-up');
+    const timer2 = document.querySelector(".p-sub__item__count-up");
     timer2.style.borderBottom = "double 5px #BDC0BA";
     timer2.innerHTML = `${countUp}<span>秒経過</span>`;
     countUp++;
-    const cleartimeoutId = setTimeout(countTime, 1000)
+    const cleartimeoutId = setTimeout(countTime, 1000);
 
-    if(correctCount === 8) {
+    if (correctCount === 8) {
       clearTimeout(cleartimeoutId);
-      timer1.innerHTML = '終了！'
+      timer1.innerHTML = "終了！";
       showResult();
     }
   }
 
-  const result = document.querySelector('.p-sub__item__result');
+  const result = document.querySelector(".p-sub__item__result");
 
   function showResult() {
-    result.innerHTML = '<span id="p-sub__item__result-title">【評価】</span><span id="p-sub__item__result-rank"></span>';
-    result.style.borderBottom = 'double 5px #BDC0BA';
-    const rank = document.getElementById('p-sub__item__result-rank');
-    if(countUp <= 35){
-      rank.innerHTML = 'S+';
-      rank.style.color = '#efBb24';}
-      else if(countUp <= 40){
-        rank.innerHTML = 'S';
-      rank.style.color = '#533d5b';
-    } else if(countUp <= 45){
-      rank.innerHTML = 'A';
-      rank.style.color = '#6d2e5b';
-    } else if (countUp <= 50){
-      rank.innerHTML = 'B';
-      rank.style.color = '#2b5f75';
+    result.innerHTML =
+      '<span id="p-sub__item__result-title">【評価】</span><span id="p-sub__item__result-rank"></span>';
+    result.style.borderBottom = "double 5px #BDC0BA";
+    const rank = document.getElementById("p-sub__item__result-rank");
+    if (countUp <= 35) {
+      rank.innerHTML = "S+";
+      rank.style.color = "#efBb24";
+    } else if (countUp <= 40) {
+      rank.innerHTML = "S";
+      rank.style.color = "#533d5b";
+    } else if (countUp <= 45) {
+      rank.innerHTML = "A";
+      rank.style.color = "#6d2e5b";
+    } else if (countUp <= 50) {
+      rank.innerHTML = "B";
+      rank.style.color = "#2b5f75";
     } else {
-      rank.innerHTML = 'C';
-      rank.style.color = '#00896c';
+      rank.innerHTML = "C";
+      rank.style.color = "#00896c";
     }
   }
 
-  function rePlay(){
-    while(cardBox.firstChild){
-      cardBox.removeChild(cardBox.firstChild)
+  function rePlay() {
+    while (cardBox.firstChild) {
+      cardBox.removeChild(cardBox.firstChild);
     }
-    const resultTitle = document.getElementById('p-sub__item__result-title');
-    if(resultTitle !== null){
-      resultTitle.innerHTML = '【前回の評価】';//
+    const resultTitle = document.getElementById("p-sub__item__result-title");
+    if (resultTitle !== null) {
+      resultTitle.textContent = "【前回の評価】"; //
     }
     countDown = 3;
     countUp = 0;
     correctCount = 0;
-    clickReplay.classList.add("stop","stop2");
-    setTimeout(gameStart,500);
-    setTimeout(clickReplay.classList.remove('stop'),500); //memo:ここらへんもっと良い方法ある気がする
+    clickReplay.classList.add("stop", "stop2");
+    setTimeout(gameStart, 500);
+    setTimeout(clickReplay.classList.remove("stop"), 500); //memo:ここらへんもっと良い方法ある気がする
   }
 }
 
-
-
 //2人プレイモードは対戦形式でポイント制 もう一つ変数を用意してあげて、剰余で条件分岐かな？
-
-
